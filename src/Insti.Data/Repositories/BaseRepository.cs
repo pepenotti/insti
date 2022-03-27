6eabf0e4e68140aed9124e51b946eb2df124f0b8
pepenotti <antonio.franchinotti@gmail.com>
2022-03-27 15:59:09 -0300

    Add Base repository

--- src/Insti.Data/Repositories/BaseRepository.cs
+++ src/Insti.Data/Repositories/BaseRepository.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Insti.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Insti.Data.Repositories
 {
-    public abstract class BaseRepository : IBaseRepository
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
     {
+        protected InstiDbContext context;
+        protected DbSet<T> dbSet;
+
+        protected BaseRepository(InstiDbContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int entityId) => Delete(GetById(entityId));
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll() => dbSet.ToList();
+
+        public T GetById(int entityId) => dbSet.SingleOrDefault(e => e.Id == entityId)!;
+
+        public void Update(T entity)
+        {
+            dbSet.Update(entity);
+            context.SaveChanges();
+        }
     }
 }
--- src/Insti.Data/Repositories/IBaseRepository.cs
+++ src/Insti.Data/Repositories/IBaseRepository.cs
@@ -4,6 +4,10 @@ namespace Insti.Data.Repositories
 {
     public interface IBaseRepository<T> where T : BaseEntity
     {
-
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int entityId);
+        T GetById(int entityId);
+        List<T> GetAll();
     }
 }
