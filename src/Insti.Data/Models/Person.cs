7861f95f152f18039199578bfc7b5c4ecce45663
pepenotti <antonio.franchinotti@gmail.com>
2022-07-09 00:06:33 -0300

    update base models

--- src/Insti.Data/Models/Person.cs
+++ src/Insti.Data/Models/Person.cs
@@ -1,7 +1,11 @@
-﻿namespace Insti.Data.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Insti.Data.Models
 {
     public abstract class Person : BaseEntity
     {
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int GenderId{ get; set; }
